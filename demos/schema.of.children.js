892ec1e74da398065b55a96013d40a8a88660fcd
hhmpro <240050497@qq.com>
2016-07-17 01:02:03 +0800

    不可在定义Schema时嵌套使用Schema

--- demos/schema.of.children.js
+++ demos/schema.of.children.js
@@ -1,16 +1,28 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var ChildSchema = new Schema({name:String});
+var ChildSchema = new Schema({name:String,age:Number});
 
-var ToySchema = new Schema({name:String,age:Number,children:[ChildSchema]});
+var ToySchema = new Schema({
+  name:String,
+  age:Number,
+  children:[
+    ChildSchema//此处不可替换为ToySchema，会报错
+  ]
+});
 var Toy = mongoose.model('Toy',ToySchema);
 var toy = new Toy({
   name:'Alibaba',
   age:16,
   children:[{
-    name:'a li lang'
+    name:'a li lang',
+    age:4,
+    children:[{//toy的children不会有children，因为ChildSchema没有定义children的Schema
+      name:'sunzi'
+    }]
   },{
-    name:'sha po lang'
+    name:'sha po lang',
+    age:3
   }]
 });
 console.log('toy:',toy);
+// ChildSchema里没有children，toy对象里也没有，即使传了children
